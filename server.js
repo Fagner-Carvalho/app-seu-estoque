f2c65122619afd0eb7dea2104d457ca1bf5601b3
Fagner-Carvalho <fagner.m.carvalho@gmail.com>
2022-05-27 11:57:20 -0300

    config express server

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('Deu certoo!');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require('express');
+const { resolve } = require('path');
+
+const app = express();
+
+app.use('/', express.static(
+  resolve(__dirname, '.build'),
+));
+
+app.listen(
+  // eslint-disable-next-line consistent-return
+  process.env.PORT || 3000, (err) => {
+    if (err) { return console.log(err); }
+
+    console.log('app running');
+  },
+);
